5ceec14b3e64f1c77f1009d708f2faea7114cae7
yebeen <yebeen53@users.noreply.github.com>
2026-01-26 11:36:35 +0900

    부트스트랩 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css';
 
 const geistSans = Geist({
